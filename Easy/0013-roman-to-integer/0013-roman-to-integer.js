eee11db4831176130811e94f6a14ca6f2f3d6993
Yunsu Bae <83539664+mniYUNSU@users.noreply.github.com>
2023-04-12 08:48:08 +0900

    Time: 133 ms (52.25%), Space: 47 MB (72.17%) - LeetHub

---
+++ Easy/0013-roman-to-integer/0013-roman-to-integer.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    const romanNumerals = {
+        'I' : 1, 'V' : 5,
+        'X' : 10, 'L' : 50,
+        'C' : 100, 'D' : 500, 
+        'M' : 1000,
+    };
+    
+    let value = 0;
+    for (let i = 0; i < s.length; i++) {
+        let currentLetter = s[i];
+        let nextLetter = s[i+1];
+        
+        if (romanNumerals[currentLetter] < romanNumerals[nextLetter]) {
+            value += romanNumerals[nextLetter] - romanNumerals[currentLetter];
+            i++;
+        }
+        else {
+            value += romanNumerals[currentLetter];
+        }
+    }
+    return value;
+};

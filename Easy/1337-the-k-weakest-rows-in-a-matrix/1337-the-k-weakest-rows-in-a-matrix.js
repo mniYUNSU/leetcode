5c17243da64e1e0df551e6e002ffaca3094c56a3
Yunsu Bae <83539664+mniYUNSU@users.noreply.github.com>
2023-04-12 15:06:51 +0900

    Time: 59 ms (87.12%), Space: 44.4 MB (46.14%) - LeetHub

--- Easy/1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.js
+++ Easy/1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.js
@@ -4,18 +4,36 @@
  * @return {number[]}
  */
 var kWeakestRows = function(mat, k) {
-    let soldiers = [];
-    for (let i = 0; i < mat.length; i++) {
-        let soldier = {index:i, value:0};
-        for (let value of mat[i]) {
-            if (value === 0) {
-                break;
-            } 
-            soldier.value++;
-        }
-        soldiers.push(soldier)
-    }
-    soldiers.sort((a,b) => a.value - b.value);
+//     let soldiers = [];
+//     for (let i = 0; i < mat.length; i++) {
+//         let soldier = {index:i, value:0};
+//         for (let value of mat[i]) {
+//             if (value === 0) {
+//                 break;
+//             } 
+//             soldier.value++;
+//         }
+//         soldiers.push(soldier)
+//     }
+//     soldiers.sort((a,b) => a.value - b.value);
     
-    return soldiers.slice(0,k).map((e) => e.index);
+//     return soldiers.slice(0,k).map((e) => e.index);
+    
+    const getPower = (row) => {
+        let power = 0;
+        for (const value of row) {
+            if (value === 0) {
+                return power;
+            } 
+            power++;
+        }
+        return power;
+    }
+    
+    let soldiers = {};
+    mat.forEach((row, idx) => {
+        soldiers[idx] = getPower(row); 
+    });
+    
+    return Object.keys(soldiers).sort((a,b) => soldiers[a] - soldiers[b]).slice(0,k);
 };

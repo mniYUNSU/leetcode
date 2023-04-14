1c53e77fa8ab68e97e1c266802fe0d88e326e9b7
Yunsu Bae <83539664+mniYUNSU@users.noreply.github.com>
2023-04-14 11:18:19 +0900

    Time: 49 ms (95.95%), Space: 41.9 MB (60.67%) - LeetHub

--- Easy/0027-remove-element/0027-remove-element.js
+++ Easy/0027-remove-element/0027-remove-element.js
@@ -4,13 +4,30 @@
  * @return {number}
  */
 var removeElement = function(nums, val) {
-    if (nums.length === 0) return 0;
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] === val) {
-            nums.splice(i,1);
-            i -= 1;
+    let left = 0, right = nums.length - 1;
+    
+    // nums[left] 값이 val 과 일치하면 nums[left] 에 nums[right] 할당후 right--
+    // 아니라면 left ++
+    // [0,1,2,2,3,0,4,2] val = 2
+    // 0 !== 2 -> left : 1 , right : 8
+    // 1 !== 2 -> left : 2 , right : 8
+    // 2 === 2 -> [0,1,2,2,3,0,4,2] left : 2 , right : 7
+    // 2 === 2 -> [0,1,4,2,3,0,4,2] left : 2 , right : 6
+    // 4 !== 2 -> left : 3 , right : 6
+    // 2 === 2 -> [0,1,4,0,3,0,4,2] left : 3 , right : 5
+    // 0 !== 2 -> left : 4 , right : 5
+    // 3 !== 2 -> left : 5 , right : 5
+    // left , right 가 같아지는 시점은 즉 nums 의 모든 값들을 탐색했다는 뜻이므로 반복 종료
+    
+    while (left <= right) {
+        if (nums[left] === val) {
+            nums[left] = nums[right];
+            right--;
+        }
+        else {
+            left++;
         }
     }
-    return nums.length;
     
+    return left;
 };

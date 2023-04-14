a901e34f145836bf3e95386c137e619e8e137e82
Yunsu Bae <83539664+mniYUNSU@users.noreply.github.com>
2023-04-14 19:42:19 +0900

    Time: 61 ms (95.40%), Space: 44.5 MB (77.45%) - LeetHub

--- Easy/0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ Easy/0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -6,6 +6,11 @@ var removeDuplicates = function(nums) {
     // 두개의 포인터 사용
     // 하나는 일반적으로 순회하는 포인터
     // 나머지 하나는 고유한 엘리먼트만 순회하는 포인터
+    
+    // nums를 0부터 끝까지 하나씩 탐색한다.
+    // nums[current] 와 nums[unique] 가 다른 경우, 중복된 엘리먼트를 현재 엘리먼트로 바꾼다. 고유한 엘리먼트의 인덱스를 증가한다.
+    // 같은 경우라면 다음 인덱스로 넘어간다.
+    // 반복이 종료되면 고유한 값을 리턴한다. 초기값을 0으로 했으므로 리턴할 때 1을 더한다.
     let current = 0, unique = 0;
     
     while (current < nums.length) {

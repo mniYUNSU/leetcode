3128b9b003b37456cba18d4ce5d2b6bac9790e65
Yunsu Bae <83539664+mniYUNSU@users.noreply.github.com>
2023-04-14 19:36:19 +0900

    Time: 68 ms (82.70%), Space: 44.7 MB (52.34%) - LeetHub

---
+++ Easy/0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    // 두개의 포인터 사용
+    // 하나는 일반적으로 순회하는 포인터
+    // 나머지 하나는 고유한 엘리먼트만 순회하는 포인터
+    let current = 0, unique = 0;
+    
+    while (current < nums.length) {
+        if (nums[current] !== nums[unique]) {
+            nums[unique+1] = nums[current];
+            unique++;
+        }
+            current++;
+    }
+    return unique+1;
+};

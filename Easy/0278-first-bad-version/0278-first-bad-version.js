b2663b4b74224f8d7283270026de258c978070db
Yunsu Bae <83539664+mniYUNSU@users.noreply.github.com>
2023-04-16 16:47:03 +0900

    Time: 58 ms (63.77%), Space: 41.8 MB (46.44%) - LeetHub

---
+++ Easy/0278-first-bad-version/0278-first-bad-version.js
@@ -0,0 +1,49 @@
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    
+    // 최초의 badversion 리턴
+    // n 의 절반부터 탐색
+    // badversion 이면 n - 1
+    // 아니면 n + 1
+    // 탐색한 n 이 badversion 인데 n-1 이 badversion이 아니면 최초의 badversion
+    return function(n) {
+        let left = 1, right = n;
+        
+        // n = 5 bad = 4
+        // left = 1
+        // right = 5
+        // mid = 2
+        
+        // [1,2,3,4,5,6,7,8] n = 8 , bad = 3
+        // left = 1   
+        // right = 8 4 
+        // mid = 4 
+        while (left < right) {
+            let mid = Math.floor(left + (right - left) / 2);
+            if (isBadVersion(mid)) {
+                if (!isBadVersion(mid-1)) return mid;
+                right = mid;
+            } else {
+                left = mid + 1;            
+            }
+        }
+        return left;
+    };
+};

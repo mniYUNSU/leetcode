cecf7bce4a955e00eda7fa7ab9f954d7c19c5681
Yunsu Bae <83539664+mniYUNSU@users.noreply.github.com>
2023-04-18 18:36:42 +0900

    Time: 82 ms (28.88%), Space: 48.7 MB (40.14%) - LeetHub

--- Easy/0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.js
+++ Easy/0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.js
@@ -6,7 +6,6 @@ var reverseWords = function(s) {
     // string 의 공백은 유지한 채로 리버스 스트링 리턴
     // ' ' 이 나오기 전 까지의 단어를 앞쪽부터 쌓는다.
     // ' ' 이 나오면 그 단어를 붙이고 초기화한다.
-    
     let result = '', stack = '';
     
     for (let i = 0; i < s.length; i++) {

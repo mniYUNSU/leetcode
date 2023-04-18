5490d1365ac686045ce061a5ac226dd9d4c1becd
Yunsu Bae <83539664+mniYUNSU@users.noreply.github.com>
2023-04-18 17:42:36 +0900

    Time: 79 ms (94.61%), Space: 49.4 MB (52.02%) - LeetHub

---
+++ Easy/0344-reverse-string/0344-reverse-string.js
@@ -0,0 +1,10 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    // 리버스한 배열 리턴
+    // js 내장 함수 사용
+    return s.reverse();
+    
+};

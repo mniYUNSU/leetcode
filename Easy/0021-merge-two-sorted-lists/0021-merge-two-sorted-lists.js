4ddb1f783f25c5eeeff47f0bda794249ca70f86e
Yunsu Bae <83539664+mniYUNSU@users.noreply.github.com>
2023-04-26 21:59:44 +0900

    Time: 59 ms (90.61%), Space: 43.8 MB (78.75%) - LeetHub

--- Easy/0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
+++ Easy/0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -29,7 +29,6 @@ var mergeTwoLists = function(list1, list2) {
         if (list1.val > list2.val) {
             pointer.next = list2;
             list2 = list2.next;
-            // 
         }
         else {
             pointer.next = list1;

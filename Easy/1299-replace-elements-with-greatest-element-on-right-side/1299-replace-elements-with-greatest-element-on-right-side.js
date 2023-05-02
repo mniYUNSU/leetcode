e09252a7b1d9c26c56e350122e97aff210f47889
Yunsu Bae <83539664+mniYUNSU@users.noreply.github.com>
2023-05-02 23:00:41 +0900

    Time: 175 ms (70.25%), Space: 51.2 MB (69.32%) - LeetHub

--- Easy/1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.js
+++ Easy/1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.js
@@ -3,11 +3,13 @@
  * @return {number[]}
  */
 var replaceElements = function(arr) {
-    let result = new Array(arr.length).fill(-1);
+    let max = -1;
     
     for (let i = arr.length - 1; i >= 0; i--) {
-        result[i-1] = Math.max(arr[i], result[i]);
+        let cur = arr[i]; // 1, 6, 4, 5, 18, 17
+        arr[i] = max; // -1, 1, 6, 6, 6, 18
+        max = Math.max(cur, max); // 1, 6, 6, 6, 18, 18
     }
     
-    return result;    
+    return arr;    
 };

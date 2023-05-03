3852d0599023c87140a3ebf3cfd45cabf38d9fc7
Yunsu Bae <83539664+mniYUNSU@users.noreply.github.com>
2023-05-03 17:22:47 +0900

    Time: 58 ms (72.63%), Space: 42.4 MB (83.68%) - LeetHub

--- Easy/1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.js
+++ Easy/1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.js
@@ -4,15 +4,16 @@
  */
 var arraySign = function(nums) {
     // nums element 를 전부 곱한 값을 singFunc 파라미터로 넣어 리턴
-    let product = 1;
+    // 전부 곱하여 판단할 수 있지만 다른 방법이 있다.
+    // element 가 0 이면 곱한 값은 무조건 0 이므로 0 리턴
+    // 음수 값의 갯수가 짝수라면 무조건 곱한 값은 양수이므로 1 리턴
+    // 그 외엔 -1 리턴
+    let negative = 0;
     for (let i = 0; i < nums.length; i++) {
-        product *= nums[i];
+        if (nums[i] === 0) return 0;
+        if (nums[i] < 0) {
+            negative++;
+        }
     }
-    return signFunc(product);
+    return negative % 2 === 0 ? 1 : -1;
 };
-
-const signFunc = (x) => {
-    if (x > 0) return 1;
-    if (x < 0) return -1;
-    return 0;
-}

bb3d22cb4b12af196afe06f738c711fd8d4f0e2d
Yunsu Bae <83539664+mniYUNSU@users.noreply.github.com>
2023-05-08 18:03:16 +0900

    Time: 65 ms (36.28%), Space: 43.6 MB (73.18%) - LeetHub

--- Easy/1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.js
+++ Easy/1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.js
@@ -6,12 +6,12 @@ var diagonalSum = function(mat) {
     // matrix의 대각선 요소들의 합
     // 중복 허용 x
     
-    // 리턴할 결과값 변수
+    // 리턴할 결과값 변수와 반대쪽 대각선 인덱스가 될 변수
     let sum = 0;
     let secondary = mat.length - 1;
     
     // matrix를 순회하며 mat[i][j] 를 sum에 더해나간다.
-    // 중복을 방지하기 위해 더한 요소는 0으로 바꾼다.
+    // 중복될 경우 (i === secondary)가 아니면 반대쪽 요소도 더한다.
     for (let i = 0; i < mat.length; i++) {
         if (i !== secondary) {
             sum += mat[i][i] + mat[i][secondary];

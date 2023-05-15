dd31b955094c7ef4cffe138ff834b7a074970e8b
Yunsu Bae <83539664+mniYUNSU@users.noreply.github.com>
2023-05-15 17:44:01 +0900

    Time: 87 ms (5.21%), Space: 45 MB (5.14%) - LeetHub

---
+++ Medium/0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function(head) {
+    // 인접한 두 노드를 스위칭 한 리스트 리턴
+    
+    let pointer = head;
+    
+    while (pointer !== null) {
+        console.log(pointer)
+        let memo = pointer.val;
+    if (pointer === null || pointer.next === null) return head;
+        pointer.val = pointer.next.val;
+        pointer.next.val = memo;
+        
+        pointer = pointer.next.next;
+    }
+    
+    return head;
+};

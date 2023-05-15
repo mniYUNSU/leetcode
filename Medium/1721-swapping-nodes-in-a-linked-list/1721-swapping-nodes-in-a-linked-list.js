892da0b8a0c5c5934c236b6c878665a3a71c2a14
Yunsu Bae <83539664+mniYUNSU@users.noreply.github.com>
2023-05-15 17:30:55 +0900

    Time: 440 ms (81.85%), Space: 101.1 MB (21.77%) - LeetHub

--- Medium/1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.js
+++ Medium/1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.js
@@ -12,7 +12,6 @@
  */
 var swapNodes = function(head, k) {
     // 처음에서 k 번째 값과 끝에서 k 번째 값을 바꾼 리스트 리턴
-    // 처음과 끝에서 시작하는 두개의 포인터 사용
     // start 포인터를 kth 노드에 위치시킨다.
     // start 포인터를 끝까지 이동시키며 end 포인터를 이동시킨다
     // 포인터가 끝에 도달했을 때 start 포인터 밸류와 end 포인터 밸류를 바꾼다
@@ -32,7 +31,4 @@ var swapNodes = function(head, k) {
     start.val = end.val;
     end.val = memo;
     return head;
-    
-    
-    
 };

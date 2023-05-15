7abb56bb6f98b8c772f63fbd63f3e4a6f735ae70
Yunsu Bae <83539664+mniYUNSU@users.noreply.github.com>
2023-05-15 17:44:59 +0900

    Time: 60 ms (48.56%), Space: 41.5 MB (95.95%) - LeetHub

--- Medium/0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.js
+++ Medium/0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.js
@@ -11,18 +11,17 @@
  */
 var swapPairs = function(head) {
     // 인접한 두 노드를 스위칭 한 리스트 리턴
-    
+    // 2씩 이동하는 포인터 생성
+    // 현재 위치와 다음 위치의 밸류를 바꾼다.
+    // 2씩 이동한다.
     let pointer = head;
     
     while (pointer !== null) {
-        console.log(pointer)
         let memo = pointer.val;
         if (pointer === null || pointer.next === null) return head;
         pointer.val = pointer.next.val;
         pointer.next.val = memo;
-        
         pointer = pointer.next.next;
     }
-    
     return head;
 };

960834d69005a44abc38ccd8524195a91d9405c9
Yunsu Bae <83539664+mniYUNSU@users.noreply.github.com>
2023-05-25 16:17:47 +0900

    Time: 51 ms (95.62%), Space: 42.3 MB (56.04%) - LeetHub

--- Easy/0771-jewels-and-stones/0771-jewels-and-stones.js
+++ Easy/0771-jewels-and-stones/0771-jewels-and-stones.js
@@ -5,11 +5,15 @@
  */
 var numJewelsInStones = function(jewels, stones) {
     // stones  중에서 jewels의 갯수 리턴
-    let count = 0;
-    for (let i = 0; i < jewels.length; i++) {
-        for (let j = 0; j < stones.length; j++) {
-            if (stones[j] === jewels[i]) count++;
-        }
-    }
-    return count;
+//     let count = 0;
+//     for (let i = 0; i < jewels.length; i++) {
+//         for (let j = 0; j < stones.length; j++) {
+//             if (stones[j] === jewels[i]) count++;
+//         }
+//     }
+//     return count;
+    
+    // set 자료구조 사용
+    const set = new Set(jewels);
+    return stones.split('').reduce((count, value) => set.has(value) ? count = count + 1 : count, 0);
 };

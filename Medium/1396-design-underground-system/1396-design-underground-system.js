63b2bf1f2fae6ba8599c55aeecf8157bd07f80bd
Yunsu Bae <83539664+mniYUNSU@users.noreply.github.com>
2023-05-31 17:14:21 +0900

    Time: 1304 ms (6.25%), Space: 66 MB (6.25%) - LeetHub

--- Medium/1396-design-underground-system/1396-design-underground-system.js
+++ Medium/1396-design-underground-system/1396-design-underground-system.js
@@ -10,6 +10,8 @@ var UndergroundSystem = function() {
  * @return {void}
  */
 UndergroundSystem.prototype.checkIn = function(id, stationName, t) {
+    // 첫 데이터라면 stationName을 key 로 한 배열을 만든다.
+    // stationName 밸류에 탑승객 id, 탑승 시간, 체크아웃 여부를 푸시한다.
     if (!this[stationName]) this[stationName] = [];
     this[stationName].push({ id, time: t, checkOut: null });
 };
@@ -21,6 +23,8 @@ UndergroundSystem.prototype.checkIn = function(id, stationName, t) {
  * @return {void}
  */
 UndergroundSystem.prototype.checkOut = function(id, stationName, t) {
+    // 체크인한 데이터 중 탑승객 id 와 일치한 데이터와 체크아웃 하지 않은 데이터를 필터링한다.
+    // 그 데이터에 체크아웃 stationName 을 넣는다.
     Object.values(this).map((key) => {
         for (let i = 0; i < key.length; i++) {
             if (key[i].id === id && key[i].checkOut === null) {
@@ -37,6 +41,8 @@ UndergroundSystem.prototype.checkOut = function(id, stationName, t) {
  * @return {number}
  */
 UndergroundSystem.prototype.getAverageTime = function(startStation, endStation) {
+    // 체크인, 체크아웃까지 모두 성공한 데이터를 필터링한다.
+    // 시간의 평균을 리턴한다.
     const checkedOutTravel = Object.values(this[startStation]).filter(travel => travel.checkOut === endStation);
     const travelTime = checkedOutTravel.reduce((a,b) => a + b.time ,0);
     return (travelTime / checkedOutTravel.length).toFixed(5);

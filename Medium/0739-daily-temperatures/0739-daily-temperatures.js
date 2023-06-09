4f4ff9fd47ca85ef343db74bc29ba41b5441e8b5
Yunsu Bae <83539664+mniYUNSU@users.noreply.github.com>
2023-06-09 23:54:30 +0900

    Time: 274 ms (76.00%), Space: 65.3 MB (84.87%) - LeetHub

--- Medium/0739-daily-temperatures/0739-daily-temperatures.js
+++ Medium/0739-daily-temperatures/0739-daily-temperatures.js
@@ -9,23 +9,41 @@ var dailyTemperatures = function(temperatures) {
     // 온도배열을 순회
     // stack에 넣고 하나씩 빼서 그 요소의 다음 인덱스부터 순회한다
     // 스택에서 뺀 값이 다음 인덱스보다 크면 일수를 늘리고 아니라면 일수 + 1 값을 할당한다.
+    // 시간이 너무 오래걸림
     
-    let stack = [];
+//     let stack = [];
+//     for (let i = 0; i < temperatures.length; i++) {
+//         let days = 0;
+//         stack.push(temperatures[i]);
+//         let ref = stack.pop();
+        
+//         temperatures[i] = 0;
+        
+//         for (let j = i+1; j < temperatures.length; j++) {
+//             if (temperatures[j] <= ref) {
+//                 days++;     
+//             } else {
+//                 temperatures[i] = days + 1;
+//                 break;
+//             }
+//         }
+//     }
+//     return temperatures;
+    
+    // 각 온도의 인덱스를 스택에 저장한다.
+    // 온도배열을 반복순회
+    // 스택이 존재하고, 현재 온도 값이 마지막으로 스택에 저장된 인덱스값 보다 클때까지 반복
+    // 스택에서 빼서 현재 인덱스 값과의 차이를 result에 업데이트한다.
+    // 스택에 인덱스를 넣는다.
+    
+    let stack = [], result = new Array(temperatures.length).fill(0);
+    
     for (let i = 0; i < temperatures.length; i++) {
-        let days = 0;
-        stack.push(temperatures[i]);
-        let ref = stack.pop();
-        
-        temperatures[i] = 0;
-        
-        for (let j = i+1; j < temperatures.length; j++) {
-            if (temperatures[j] <= ref) {
-                days++;     
-            } else {
-                temperatures[i] = days + 1;
-                break;
-            }
+        while (stack.length && temperatures[i] > temperatures[stack[stack.length - 1]]) {
+            let temp = stack.pop();
+            result[temp] = i - temp;
         }
+        stack.push(i);
     }
-    return temperatures;
+    return result;
 };

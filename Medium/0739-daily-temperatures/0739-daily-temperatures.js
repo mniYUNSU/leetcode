c6486143dff4a0cf8fed473777a37cc97b3f46df
Yunsu Bae <83539664+mniYUNSU@users.noreply.github.com>
2023-06-09 23:41:53 +0900

    Time: 7849 ms (5.06%), Space: 65.3 MB (84.13%) - LeetHub

--- Medium/0739-daily-temperatures/0739-daily-temperatures.js
+++ Medium/0739-daily-temperatures/0739-daily-temperatures.js
@@ -7,6 +7,9 @@ var dailyTemperatures = function(temperatures) {
     // 더 따뜻한 날씨가 될 때 까지 몇일을 기다려야하는지 구하여 리턴
     
     // 온도배열을 순회
+    // stack에 넣고 하나씩 빼서 그 요소의 다음 인덱스부터 순회한다
+    // 스택에서 뺀 값이 다음 인덱스보다 크면 일수를 늘리고 아니라면 일수 + 1 값을 할당한다.
+    
     let stack = [];
     for (let i = 0; i < temperatures.length; i++) {
         let days = 0;
@@ -21,13 +24,8 @@ var dailyTemperatures = function(temperatures) {
             } else {
                 temperatures[i] = days + 1;
                 break;
-                // ref.push(temperatures[i]);  
             }
-            
         }
-        
     }
-    
-    console.log(temperatures)
     return temperatures;
 };

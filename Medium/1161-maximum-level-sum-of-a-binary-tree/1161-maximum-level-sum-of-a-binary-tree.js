3e14f9907f7102f45130f394208fbd9243a22e96
Yunsu Bae <83539664+mniYUNSU@users.noreply.github.com>
2023-06-15 21:50:01 +0900

    Time: 174 ms (31.71%), Space: 76.8 MB (15.12%) - LeetHub

--- Medium/1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.js
+++ Medium/1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.js
@@ -12,10 +12,6 @@
  */
 var maxLevelSum = function(root) {
     // 같은 레벨에 있는 노드 총합이 가장 큰 레벨 리턴
-    // 노드 순회하며 현재 노드를 큐에 넣는다.
-    // 큐에서 하나씩 빼며 그 노드 값을 누적한다.
-    // 큐에서 뺀 노드의 left 와 right를 큐에 넣는다.
-    // 큐가 빌 때 더해진 누적 값을 저장해두고, 누적값이 업데이트될 때마다 대소비교
     
     let maxLevelSum = Number.MIN_SAFE_INTEGER, maxLevel = 1;
     let queue = [root], level = 1;

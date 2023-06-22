b8c45b3e9a081767e5c4fe9637e10b80e4ec257b
Yunsu Bae <83539664+mniYUNSU@users.noreply.github.com>
2023-06-22 22:03:40 +0900

    Time: 81 ms (76.70%), Space: 48.8 MB (87.86%) - LeetHub

--- Medium/0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.js
+++ Medium/0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.js
@@ -8,14 +8,20 @@ var maxProfit = function(prices, fee) {
     // 팔 때마다 fee 만큼 깎인다.
     // 최대 이익 리턴
     
+    // 주식을 hold 중일 때의 손익
+    // 주식을 갖고있지 않을 때(sold)의 손익
+    // 무조건 사야 팔 수 있으므로 hold의 초기값은 -price[0]
+    let hold = -prices[0], sold = 0;
     
-    let buy = -Infinity, sell = 0;
-    // 이미 구매했다면 -> 가만히 있거나, 팔거나 두가지
-    // 구매하지 않았다면 -> 가만히 있거나, 사거나 두가지
-    for (let price of prices) {
-        buy = Math.max(buy, sell - price);
-        sell = Math.max(sell, buy + price - fee);
+    // 주식이 없는 상태에서 가능한 경우는 가만히 있거나, 사거나
+    // 주식이 있는 상태에서 가능한 경우는 가만히 있거나, 팔거나
+    for (let i = 1; i < prices.length; i++) {
+        // 주식이 없는 상태의 최대 이익 : 주식이 없는 상태의 손익(가만히 있는다)과 주식이 있는 상태에서 현재 가격에 팔았을 때 이익 비교
+        sold = Math.max(sold, hold + prices[i] - fee);
+        // 주식이 있는 상태의 최대 이익 : 주식을 산 상태의 손익(가만히 있는다)과 주식이 없는 상태에서 현재 가격에 다시 샀을 때의 이익 비교
+        hold = Math.max(hold, sold - prices[i]);        
     }
     
-    return sell;
+    // 주식이 없는 상태의 값(팔아야 이익) 리턴
+    return sold;
 };

e2ee7a25658f1cebb512722b547ea80a730af150
Yunsu Bae <83539664+mniYUNSU@users.noreply.github.com>
2023-06-26 18:09:10 +0900

    Time: 74 ms (30.81%), Space: 45.2 MB (63.39%) - LeetHub

---
+++ Easy/0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    // 바이너리 트리의 뎁스 리턴
+    // bfs
+    if (!root) return 0;
+    // 리턴할 depth 변수, bfs를 위한 큐 생성
+    let depth = 0, queue = [root];
+    
+    // 루프를 돌며 left, right 값이 있다면 큐에 넣는다.
+    // 넣을 때 뎁스를 1 증가한다.
+    // 큐가 비면 루프 종료
+    while (queue.length > 0) {
+        const size = queue.length;
+        for (let i = 0; i < size; i++) {
+            const node = queue.shift();
+            if (node.right) {
+                queue.push(node.right);
+            } 
+            if (node.left) {
+                queue.push(node.left);
+            }
+        }        
+        depth++;
+    }
+    
+    return depth;
+};

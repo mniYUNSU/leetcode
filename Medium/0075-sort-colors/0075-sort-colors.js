6dc1825d0db30a47fff88749745ee0a9b32643c0
Yunsu Bae <83539664+mniYUNSU@users.noreply.github.com>
2023-06-28 22:05:27 +0900

    Time: 54 ms (84.88%), Space: 41.8 MB (74.50%) - LeetHub

--- Medium/0075-sort-colors/0075-sort-colors.js
+++ Medium/0075-sort-colors/0075-sort-colors.js
@@ -11,6 +11,8 @@ var sortColors = function(nums) {
     let start = 0, end = nums.length - 1;
     
     // nums 를 순회하며 nums[i] 가 0 이거나 2이면 현재 위치와 바꾼다
+    // 2와 바꾼 경우, 그 자리를 다시 탐색해야하므로 같은 인덱스를 다시 판단한다.
+    // 0과 바꾼 경우, 바꾼 자리는 반드시 0이므로 다음 인덱스로 넘어간다.
     for (let i = 0; i <= end; i++) {
         const current = nums[i];
         if (nums[i] === 0) {
@@ -20,13 +22,10 @@ var sortColors = function(nums) {
             start++;
         } else if (nums[i] === 2) {
             const prev = nums[end];
-            console.log(current,prev)
             nums[end] = current;
             nums[i] = prev;
             i--;
             end--;
         }
     }
-    
-    console.log(nums)
 };
